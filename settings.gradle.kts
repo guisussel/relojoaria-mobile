f56fde267b3a654b28f00eaf2f4491cc88b5ed72
Guilherme Sussel <guilherme.sussel@fast2mine.com>
2023-07-30 03:23:31 -0300

    created project KMM

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RelojoariaSusselMobile"
+include(":Relojoaria_Sussel")
+include(":shared")
